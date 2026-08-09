b1f9ae06633f422e9c449f3c30347d6b8778ea58
0xArCHDeViL <mczerodevil@gmail.com>
2026-08-09 12:41:44 +0000

    refactor: remove redundant floating dictionary search FAB

--- core/src/commonMain/kotlin/ua/syt0r/kanji/presentation/screen/main/MainScreen.kt
+++ core/src/commonMain/kotlin/ua/syt0r/kanji/presentation/screen/main/MainScreen.kt
@@ -26,7 +26,6 @@ import ua.syt0r.kanji.presentation.dialog.VersionChangeDialog
 import ua.syt0r.kanji.presentation.getMultiplatformViewModel
 import ua.syt0r.kanji.presentation.screen.main.features.CommandPaletteOverlay
 import ua.syt0r.kanji.presentation.screen.main.features.DeepLinkHandler
-import ua.syt0r.kanji.presentation.screen.main.features.FloatingDictionaryOverlay
 import ua.syt0r.kanji.presentation.screen.main.features.KaiteyoDataCenter
 import ua.syt0r.kanji.presentation.screen.main.features.KaiteyoPalette
 import ua.syt0r.kanji.presentation.screen.main.features.MigrationDialog
@@ -172,7 +171,6 @@ fun MainScreen(
     }
 
     CommandPaletteOverlay()
-    FloatingDictionaryOverlay()
 
     deepLinkHandler.HandleDeepLinksLaunchedEffect(navigationState)
 
